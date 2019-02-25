74caa1a88c233fd622361004e3012b9786d700c6
Quinn Krug <quinn.krug@gmail.com>
2019-02-25 16:58:50 -0700

    Set up tests

---
+++ spec/model/favorite_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Favorite, type: :model do
+
+  describe 'Relationships' do
+    it { should belong_to :user }
+  end
+
+end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  
+  describe 'Relationships' do
+    it { should have_many(:favorites) }
+  end
+
+end
