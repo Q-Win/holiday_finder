80bf3f2a44516e86961d557daddebfcf386b63dd
Quinn Krug <quinn.krug@gmail.com>
2019-02-26 11:20:30 -0700

    titleize serach

--- app/controllers/holiday_controller.rb
+++ app/controllers/holiday_controller.rb
@@ -1,8 +1,8 @@
 class HolidayController < ApplicationController
   def index
     facade = HolidayFacade.new
-    @theme = params["theme"]
-    @holidays = facade.search_for_a_theme(params["theme"])
+    @theme = params["theme"].titleize
+    @holidays = facade.search_for_a_theme(@theme)
 
   end
 end
