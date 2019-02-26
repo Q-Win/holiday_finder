8d8a91e3c85c68a1ee5cee2e11c1d6e0b2fe43d2
Quinn Krug <quinn.krug@gmail.com>
2019-02-26 15:37:22 -0700

    users able to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
-    # session[:user_id] = user.id
-    # redirect_to root_path
+    if @user = User.find_by(name: params["name"])
+      session[:user_id] = @user.id
+      @favorites = @user.favorites
+    else
+      flash[:error] = 'Incorrect user name. Pleas try again.'
+      redirect_to root_path
+    end
   end
 end
